f950d9fa2a787819d831820a5d54af81bb6a98a8
Morgan Moss <morgan@bbd.co.za>
2023-05-16 18:31:37 +0200

    /workout & /add_exercise

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const session = require('express-session');
+const ejs = require('ejs');
 const passport = require('passport');
+const dal = require('./workoutDAL');
 require('./auth');
+const date = require('date-and-time')
 
 //TODO: Extract this function to a separate file
 function isLoggedIn(req, res, next) {
-    req.user? next() : res.sendStatus(401);
+    req.user ? next() : res.sendStatus(401);
 }
 
 const app = express();
@@ -15,19 +18,50 @@ const SESSION_SECRET = 'secret';
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.static(__dirname + '\\public'));
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
 
 // Routes
 app.get('/', (req, res) => {
-    //res.sendFile(__dirname + '/static/workoutPage.html');
     res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
 
 app.get('/workout', isLoggedIn, (req, res) => {
-    res.send(`<p>Welcome ${req.user.displayName}</p> <a href="/logout">Logout</a>`);
+    let workout = dal.getWorkoutsForUser(0).workouts[0]
+    workout.date = date.format(workout.date, "dddd, D MMM") 
+    let exercises = dal.getExercisesForWorkout(workout.id).exercises
+    let grouped_exercises = {}
+
+    for (let exercise of exercises) {
+        if (!(exercise.name in grouped_exercises)) {
+            grouped_exercises[exercise.name] = []
+        } 
+        
+        let exercise_group = grouped_exercises[exercise.name]
+
+        let set = {
+            number: exercises.filter(x => x.name == exercise.name).length - exercise_group.length,
+            metrics: exercise.metrics
+        }
+
+        exercise_group.push(set)
+        grouped_exercises[exercise.name] = exercise_group
+    }
+
+    let user = req.user.displayName
+
+    res.render('workoutPage', {
+        workout: workout,
+        exercises: grouped_exercises,
+        user: user
+    });
+
 });
 
 app.get('/logout', (req, res) => {
-    req.logout(function(err) {
+    req.logout(function (err) {
         if (err) {
             res.send('An error occurred while logging you out.');
         } else {
@@ -40,8 +74,8 @@ app.get('/logout', (req, res) => {
 
 // Google authentication routes
 app.get('/auth/google',
-    passport.authenticate('google', { scope: [ 'email', 'profile' ] }
-));
+    passport.authenticate('google', { scope: ['email', 'profile'] }
+    ));
 
 app.get('/auth/google/callback',
     passport.authenticate('google', {
@@ -53,6 +87,11 @@ app.get('/auth/google/callback',
 app.get('auth/google/failure', (req, res) => {
     res.send('Something went wrong with Google authentication');
 });
+
+app.post("/add_exercise", (req, res) => {
+    res.status(200)
+    res.end()
+})
 
 // Start server
 app.listen(3000, function () {
