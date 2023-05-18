b3d6af54f5fec11cc438cbfb589a40713a58497b
JordanFalkBBD <jordanf@bbd.co.za>
2023-05-18 14:31:54 +0200

    Added import

--- controllers/workout.js
+++ controllers/workout.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const dal = require("../models/workoutDAL");
 
 router.get("/", (req, res) => {
   res.sendFile(
     path.join(__dirname, "../views/components/workout-page/workoutPage.html")
   );
 });
+
 router.get("/next", async function (req, res) {
   // Retrieve the tag from our URL path
   let workouts = dal.getWorkoutsForUser(req.session.userID).workouts;
