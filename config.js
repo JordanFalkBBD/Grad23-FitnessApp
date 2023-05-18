99c1983324bc5a5a8247383774447b153b1608c4
JordanFalkBBD <jordanf@bbd.co.za>
2023-05-18 20:42:28 +0200

    Moved ninja api key

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 var config = {};
 
-
-config.ninja_key = "";
+config.ninja_key = "ggutd45z+JF1zHIFemjfpQ==S5eWla6TxzazEdqO";
 config.port = 3000;
 
 module.exports = config;
--- views/components/workout-page/workoutPage.js
+++ views/components/workout-page/workoutPage.js
@@ -1,6 +1,3 @@
-// TODO: extract to env or config
-const NINJA_API_KEY = "ggutd45z+JF1zHIFemjfpQ==S5eWla6TxzazEdqO";
-
 async function getExercises() {
   return fetch("/exercises").then((response) => response.json());
 }
