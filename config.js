4ef8c3f2c03308bf9708ddce4284e0f7db7363cd
Ryan Parker <ryanneilparker@gmail.com>
2023-05-22 15:44:13 +0200

    Add session secret to config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ config.google_client_id = "48579065357-mkqbtlvpfs2fkh1r0vpk32etc1rgnsad.apps.goo
 config.google_client_secret = "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";
 config.google_callback_url = "http://localhost:3000/auth/google/callback";
 
+config.session_secret = "secret"
 
 module.exports = config;
