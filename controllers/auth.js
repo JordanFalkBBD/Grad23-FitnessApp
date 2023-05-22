8d3608320ecbde496be49b505facd3a0fd2dcb81
Ryan Parker <ryanneilparker@gmail.com>
2023-05-22 15:49:04 +0200

    Prettify auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,46 +5,43 @@ const GoogleStrategy = require("passport-google-oauth2").Strategy;
 require('https').globalAgent.options.rejectUnauthorized = false;
 const config = require("../config");
 
+// Google authentication
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: config.google_client_id,
-      clientSecret: config.google_client_secret,
-      callbackURL: config.google_callback_url,
-      passReqToCallback: true,
-    },
-    function (request, accessToken, refreshToken, profile, done) {
-        // TODO: add/associate user to db records
-      return done(null, profile);
-    }
-  )
+    new GoogleStrategy(
+        {
+            clientID: config.google_client_id,
+            clientSecret: config.google_client_secret,
+            callbackURL: config.google_callback_url,
+            passReqToCallback: true,
+        },
+        function (request, accessToken, refreshToken, profile, done) {
+            // TODO: add/associate user to db records
+            return done(null, profile);
+        }
+    )
 );
 
+// Serialization and deserialization of users
 passport.serializeUser(function (user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function (user, done) {
-  done(null, user);
+    done(null, user);
 });
 
-router.get(
-  "/google",
-  passport.authenticate("google", {
+// Routes
+router.get("/google", passport.authenticate("google", {
     scope: ["email", "profile"],
-  })
-);
+}));
 
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
+router.get("/google/callback", passport.authenticate("google", {
     successRedirect: "/workout",
     failureRedirect: "/auth/google/failure",
-  })
-);
+}));
 
 router.get("/google/failure", (req, res) => {
-  res.send("Something went wrong with Google authentication");
+    res.send("Something went wrong with Google authentication");
 });
 
 module.exports = router;
