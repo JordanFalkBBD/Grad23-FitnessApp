ff3f5d878934636f4aaa2aa90c4ac5b0613edbad
Ryan Parker <ryanneilparker@gmail.com>
2023-05-22 15:29:52 +0200

    Add google secrets to config.js

--- config.js
+++ config.js
@@ -3,4 +3,9 @@ var config = {};
 config.ninja_key = "ggutd45z+JF1zHIFemjfpQ==S5eWla6TxzazEdqO";
 config.port = 3000;
 
+config.google_client_id = "48579065357-mkqbtlvpfs2fkh1r0vpk32etc1rgnsad.apps.googleusercontent.com";
+config.google_client_secret = "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";
+config.google_callback_url = "http://localhost:3000/auth/google/callback";
+
+
 module.exports = config;
