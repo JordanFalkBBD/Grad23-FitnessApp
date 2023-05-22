b0b01549bcb47c2ee9bdb5ebe8a986696cb1b89e
JordanFalkBBD <jordanf@bbd.co.za>
2023-05-22 18:59:17 +0200

    Added route for api call

--- controllers/summary.js
+++ controllers/summary.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const dal = require("../models/Server");
 
 router.get("/", (req, res) => {
   res.sendFile(
     path.join(__dirname, "../views/components/summary-page/summaryPage.html")
   );
+});
+
+router.get("/exercises/:userID", (req, res) => {
+  dal
+    .fetchExercisesForUser(req.params.userID)
+    .then((response) => res.json(response));
 });
 
 module.exports = router;
